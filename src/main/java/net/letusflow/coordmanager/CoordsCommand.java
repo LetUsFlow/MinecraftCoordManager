f5e9358156b629923fc3cde8560d2d0b5e19eed5
Florentin Schäfer <florentin.schaefer@gmail.com>
2024-02-05 16:33:39 +0100

    add tab completion for add command

--- src/main/java/net/letusflow/coordmanager/CoordsCommand.java
+++ src/main/java/net/letusflow/coordmanager/CoordsCommand.java
@@ -12,6 +12,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -46,9 +47,23 @@ public class CoordsCommand implements CommandExecutor, TabCompleter {
                                     pstmt = conn.prepareStatement("INSERT INTO coordinates (uuid, note, coordx, coordy, coordz) VALUES (?,?,?,?,?)");
                                     pstmt.setString(1, player.getUniqueId().toString());
                                     pstmt.setString(2, strings[1]); // note
-                                    pstmt.setInt(3, Integer.parseInt(strings[2])); // x
-                                    pstmt.setInt(4, Integer.parseInt(strings[3])); // y
-                                    pstmt.setInt(5, Integer.parseInt(strings[4])); // z
+
+                                    if (strings[2].equals("~")) { // x
+                                        pstmt.setInt(3, (int)player.getX());
+                                    } else {
+                                        pstmt.setInt(3, Integer.parseInt(strings[2]));
+                                    }
+                                    if (strings[3].equals("~")) { // y
+                                        pstmt.setInt(4, (int)player.getY());
+                                    } else {
+                                        pstmt.setInt(4, Integer.parseInt(strings[3]));
+                                    }
+                                    if (strings[4].equals("~")) { // z
+                                        pstmt.setInt(5, (int)player.getZ());
+                                    } else {
+                                        pstmt.setInt(5, Integer.parseInt(strings[4]));
+                                    }
+
                                     pstmt.executeUpdate(); // TODO: handle return value
                                     return true;
 
@@ -88,8 +103,44 @@ public class CoordsCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (args.length <= 1) {
+        System.out.println(Arrays.toString(args));
+        if (args.length == 1) {
             return List.of("list", "add", "remove");
+        }
+        if (args.length >= 2 && args[0].equals("add")) {
+            if (args.length == 2) {
+                return List.of(); // ignore note
+            }
+            if (args.length == 3) {
+                if (args[2].isEmpty()) {
+                    return List.of("~ ~ ~", "~ ~ ", "~ ");
+                }
+
+                try {
+                    int coordx = Integer.parseInt(args[2]);
+                    return List.of(coordx + " ~ ~", coordx + " ~ ");
+                } catch (NumberFormatException nfe) {
+                    return List.of();
+                }
+            }
+
+            if (args.length == 4) {
+                if (args[3].isEmpty()) {
+                    return List.of("~ ~", "~ ");
+                }
+
+                try {
+                    Integer.parseInt(args[2]);
+                    int coordy = Integer.parseInt(args[3]);
+                    return List.of(coordy + " ~");
+                } catch (NumberFormatException nfe) {
+                    return List.of();
+                }
+            }
+
+            if (args.length == 5 && args[4].isEmpty()) {
+                return List.of("~");
+            }
         }
         return List.of();
     }
